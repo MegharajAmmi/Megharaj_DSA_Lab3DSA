e3946ff69bba6f3ea7870246e22284bfa1dda0e5
MegharajAmmi <123808386+MegharajAmmi@users.noreply.github.com>
2023-04-25 19:40:03 +0530

    All Files Uploaded Successfully

---
+++ Megharaj_DSA_Lab3DSA/GLDSA3Prg2/src/com/gl/node/dsa3prg2/Nodes.java
@@ -0,0 +1,14 @@
+package com.gl.node.dsa3prg2;
+
+
+public class Nodes {
+    public int data;
+    public Nodes left;
+    public Nodes right;
+
+    public Nodes(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
